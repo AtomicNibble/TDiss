51652bc2629dfb634fb9fb0d9b76b146cad3a521
Tom <tom@tom-crowley.co.uk>
2024-04-08 09:54:32 +0100

    Don't force system

--- premake5.lua
+++ premake5.lua
@@ -6,11 +6,9 @@ workspace "TDiss"
     language "C++"
 
     filter { "platforms:Win32*" }
-        system "Windows"
         architecture "x86"
 
     filter { "platforms:Win64*" }
-        system "Windows"
         architecture "x86_64"
 
     filter { "configurations:Debug" }
