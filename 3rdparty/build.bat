e5333c4d5984f62e6ae2d455b6b280ab8abe0120
Tom <tom@tom-crowley.co.uk>
2024-04-08 12:50:11 +0100

    Build 3rd party libs for linux

--- 3rdparty/build.bat
+++ 3rdparty/build.bat
@@ -1,11 +1,11 @@
 
 
 premake5 vs2022
-msbuild /m thirdparty.sln /t:build /p:Configuration=Debug;Platform=Win32
-msbuild /m thirdparty.sln /t:build /p:Configuration=Release;Platform=Win32
-msbuild /m thirdparty.sln /t:build /p:Configuration=Ship;Platform=Win32
+msbuild /m thirdparty.sln /t:build /p:Configuration=Debug;Platform=x86
+msbuild /m thirdparty.sln /t:build /p:Configuration=Release;Platform=x86
+msbuild /m thirdparty.sln /t:build /p:Configuration=Ship;Platform=x86
 
-msbuild /m thirdparty.sln /t:build /p:Configuration=Debug;Platform=Win64
-msbuild /m thirdparty.sln /t:build /p:Configuration=Release;Platform=Win64
-msbuild /m thirdparty.sln /t:build /p:Configuration=Ship;Platform=Win64
+msbuild /m thirdparty.sln /t:build /p:Configuration=Debug;Platform=x64
+msbuild /m thirdparty.sln /t:build /p:Configuration=Release;Platform=x64
+msbuild /m thirdparty.sln /t:build /p:Configuration=Ship;Platform=x64
 
