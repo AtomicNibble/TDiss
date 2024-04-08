908b2c96147ac338201e49cc03b520fc06763800
Tom <tom@tom-crowley.co.uk>
2024-04-08 12:44:38 +0100

    Fix case of filename

--- TDiss/TDissLogger.h
+++ TDiss/TDissLogger.h
@@ -41,12 +41,16 @@ namespace TDiss
 
 #define TDISS_LOGGING 1
 
-#define TDISS_LOG0(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL0, pFmt, __VA_ARGS__); }
-#define TDISS_LOG1(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL1, pFmt, __VA_ARGS__); }
-#define TDISS_LOG2(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL2, pFmt, __VA_ARGS__); }
+// fixes expansion for gcc
+#define X_DO_LOG_VA_ARGS(...) , ##__VA_ARGS__ 
 
-#define TDISS_WARN(pFmt, ...) if (pLogger_) { pLogger_->Warning(pFmt, __VA_ARGS__); }
-#define TDISS_ERR(pFmt, ...) if (pLogger_) { pLogger_->Error(pFmt, __VA_ARGS__); }
+
+#define TDISS_LOG0(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL0, pFmt X_DO_LOG_VA_ARGS(__VA_ARGS__)); }
+#define TDISS_LOG1(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL1, pFmt X_DO_LOG_VA_ARGS(__VA_ARGS__)); }
+#define TDISS_LOG2(pFmt, ...) if (pLogger_) { pLogger_->Log(IDissLogger::Verbosity::LVL2, pFmt X_DO_LOG_VA_ARGS(__VA_ARGS__)); }
+
+#define TDISS_WARN(pFmt, ...) if (pLogger_) { pLogger_->Warning(pFmt X_DO_LOG_VA_ARGS(__VA_ARGS__)); }
+#define TDISS_ERR(pFmt, ...) if (pLogger_) { pLogger_->Error(pFmt X_DO_LOG_VA_ARGS(__VA_ARGS__)); }
 
 #endif
 
