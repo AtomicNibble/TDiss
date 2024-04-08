c027449bf33cdb110241011b88d3f36e400378fd
Tom <tom@tom-crowley.co.uk>
2024-04-08 09:53:58 +0100

    Add unix version of lastError

--- CommonLib/Util/LastError.cpp
+++ CommonLib/Util/LastError.cpp
@@ -3,28 +3,71 @@
 
 namespace lastError
 {
-	unsigned int Get(void) {
+#if X_WIN32
+
+	ErrorInt Get(void)
+	{
 		return ::GetLastError();
 	}
 
 	const char* ToString(DWORD error, Description& desc)
 	{
-		zero_object(desc);
+        desc[0] = '\0';
 
-		DWORD size = ::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM,	// It´s a system error
-			NULL,													// No string to be formatted needed
-			error,
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),				// Do it in the standard language
-			desc,													// Put the message here
-			sizeof(Description),									// Number of bytes to store the message
-			NULL);
+        Description tmp;
 
-		desc[size] = '\0';
-		return desc;
+        DWORD size = ::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, // It's a system error
+            NULL,                                                 // No string to be formatted needed
+            error,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Do it in the standard language
+            tmp,                                       // Put the message here
+            sizeof(tmp),                               // Number of bytes to store the message
+            NULL);
+
+        if (size == 0) {
+            return desc;
+        }
+        // remove \n
+        if (size > 1) {
+            tmp[size - 2] = '\0';
+        }
+
+        int err = snprintf(desc, sizeof(desc), "Error(0x%08x): %s", error, tmp);
+        if (err < 0) {
+            std::strncpy(desc, "Failed to encode error message", sizeof(desc));
+        }
+
+        return desc;
 	}
 
 	const char* ToString(Description& desc)
 	{
 		return ToString(::GetLastError(), desc);
 	}
+
+#else // X_WIN32
+
+    ErrorInt Get(void)
+    {
+        return errno;
+    }
+
+    const char* ToString(ErrorInt error, Description& desc)
+    {
+        desc[0] = '\0';
+
+        Description tmp;
+        const char* pErrStr = ::strerror_r(error, tmp, safe_static_cast<int>(sizeof(tmp)));
+
+        snprintf(desc, sizeof(desc), "Error(0x%08x): %s", error, pErrStr);
+        return desc;
+    }
+
+    const char* ToString(Description& desc)
+    {
+        return ToString(Get(), desc);
+    }
+
+#endif // X_WIN32
+
 }
--- CommonLib/Util/LastError.h
+++ CommonLib/Util/LastError.h
@@ -2,9 +2,15 @@
 
 namespace lastError
 {
+#ifdef X_WIN32
+    using ErrorInt = uint32_t;
+#else // X_WIN32
+    using ErrorInt = int32_t;
+#endif// X_WIN32
+
 	typedef char Description[512];
 
-	unsigned int Get(void);
-	const char* ToString(DWORD error, Description& desc);
-	const char* ToString(Description& desc);
-}
+	ErrorInt Get(void);
+	X_NO_INLINE char* ToString(ErrorInt error, Description& desc);
+	X_NO_INLINE char* ToString(Description& desc);
+} // namespace lastError
